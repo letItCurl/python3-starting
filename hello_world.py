f29cb3cbddcdf06b974c4639e366d7fcb75eec0f
letItCurl <rolandlopez.developer@gmail.com>
2020-09-30 09:21:19 +0200

    hello_world

--- hello_world.py
+++ hello_world.py
@@ -10,10 +10,12 @@ age = input("What is your age?: ") # return int !
 city = input("From where city are you from?: ")
 love = input("What do you love?: ")
 
-print(name)
-print(age)
-print(city)
-print(love)
+# hello world !
+string = "you are {}, your age is {}, you live in {} and you love {}".format(name,age,city,love)
+print(string)
+
+# yes like ruby !
+print("="*20)
 
 # string concatenetion
 A = "part one"
@@ -21,8 +23,7 @@ B = "part two"
 C = A + B
 print(C)
 
-# yes like ruby !
-print("="*20)
+
 
 # int to str | str to int
 print('1' + str(21))
@@ -31,4 +32,8 @@ print(21 + int("1"))
 # format
 D = "{0} , {1} and again the first one {0}, then {2}".format(A,B,name)
 print(D)
+
+# yes like ruby !
+print("="*20)
+
 
